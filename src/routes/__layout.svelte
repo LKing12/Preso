e0be1ad090c04660ca1e37880aa28de9af59c73e
liamk2103 <liaking@deloitte.com.au>
2022-07-18 16:51:46 +1000

    not getting anywhere with this

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,22 @@
 <script>
     import Notifications from 'svelte-notifications';
     import Deck from '../lib/Deck.svelte';
+    import { onMount } from 'svelte';
+    onMount(async () => {
+        
+        if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('testserviceworker.js', { scope: '/' }).then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}; 
+
+
+
+    }); 
 </script>
 
 <Notifications>
